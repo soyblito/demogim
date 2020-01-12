e17d1844c2746a0124a0e5cd698b8fdb36f8fd85
soyblito <pablomisfit@gmail.com>
2020-01-12 18:52:43 -0300

    home & contact containers

--- src/App.js
+++ src/App.js
@@ -2,36 +2,21 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+
+import Navigation from './components/Navigation';
+import Home from './containers/Home';
+import Contact from './containers/Contact';
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
+      <Navigation /> 
+        <Switch>          
+          <Route path="/contacto">
+            <Contact />
           </Route>
           <Route path="/">
             <Home />
@@ -40,16 +25,4 @@ export default function App() {
       </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
